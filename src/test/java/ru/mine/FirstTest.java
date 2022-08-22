eb334bca7b403798ab4a36849a9359f48b8466b2
Marina-creator <abakumova.ma.le@gmail.com>
2022-08-22 18:33:07 +0300

    add full test forma

--- src/test/java/ru/mine/FirstTest.java
+++ src/test/java/ru/mine/FirstTest.java
@@ -4,15 +4,63 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class FirstTest {
 
     @BeforeAll
     static void setUp() {
         Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 10000;
+    //    Configuration.browserSize = "1366 x 768";
+
     }
 
     @Test
-    void testForm() {
+    void testRegistrationForm() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").val("Marina");
+        $("#lastName").val("Abakumova");
+        $("#userEmail").val("Abakumova@mine.ru");
+        $("#userNumber").val("9998887766");
+        $("#currentAddress").val("Georgia, Batumi");
+
+        $("#gender-radio-2").parent().click();
+        $("#hobbies-checkbox-2").parent().click();
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__year-select").selectOption("1992");
+        $(".react-datepicker__day--017:not(.react-datepicker__day--outside-month").click();
+
+        $("#subjectsInput").setValue("Economics").pressEnter();
+
+        $("#uploadPicture").uploadFromClasspath("picture/1.jpg");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText ("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText ("Karnal")).click();
+
+        $("#submit").click();
+
+        sleep(10000);
+
+
+
+
+
+
+
+
 
     }
 
